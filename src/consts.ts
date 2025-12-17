89467de30f59fbef0856495dc2291f4a67f2f907
Raymond <raymond.luke.spartz@gmail.com>
2025-12-17 16:35:54 -0600

    .

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Raymondreamer | Design Engineer & Creative Developer';
+export const SITE_DESCRIPTION = 'Design Engineer with 15+ years of design experience, specializing in immersive web experiences, 3D visualization, data visualization, and high-performance applications. Creator of Rthmn and Reality Designers.';
